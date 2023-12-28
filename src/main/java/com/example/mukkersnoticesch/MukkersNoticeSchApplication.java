bc32a8e6dff3b0d8b76bbb810e5d83a28d794daa
hyun2425 <hsj@infotech.co.kr>
2023-12-28 16:53:57 +0900

    1st  commit

---
+++ src/main/java/com/example/mukkersnoticesch/MukkersNoticeSchApplication.java
@@ -0,0 +1,13 @@
+package com.example.mukkersnoticesch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukkersNoticeSchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MukkersNoticeSchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mukkersnoticesch/MukkersNoticeSchApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.mukkersnoticesch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukkersNoticeSchApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+
+
+	}
+
+}
