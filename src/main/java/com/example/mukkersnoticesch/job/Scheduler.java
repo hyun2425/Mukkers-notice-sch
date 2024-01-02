041053d3398e5812ae99d33e8f235b2356bd6301
hyun2425 <hsj@infotech.co.kr>
2024-01-02 17:36:47 +0900

    1st  commit

--- src/main/java/com/example/mukkersnoticesch/job/Scheduler.java
+++ src/main/java/com/example/mukkersnoticesch/job/Scheduler.java
@@ -21,15 +21,20 @@ import java.util.List;
 public class Scheduler {
 
     //    @Scheduled(cron = "0 0/3 * * * ?")
-    @Scheduled(fixedDelay = 1000 * 60 * 60 * 3)
+//    @Scheduled(fixedDelay = 1000 * 60 * 60 * 3)
+    @Scheduled(cron = "0 0/3 8-21 * * ?")
     public void myScheduledTask() {
         log.info("sch start");
 
-        targetScraping("노원중", "https://nowon.sen.es.kr/10546/subMenu.do", "https://nowon.sen.es.kr/dggb/module/board/selectBoardListAjax.do", "BBSMSTR_000000002891");
+        targetScraping("노원중", "https://nowon.sen.ms.kr/10546/subMenu.do", "https://nowon.sen.ms.kr/dggb/module/board/selectBoardListAjax.do", "BBSMSTR_000000002891");
         targetScraping("중계중", "https://junggye.sen.ms.kr/9800/subMenu.do", "https://junggye.sen.ms.kr/dggb/module/board/selectBoardListAjax.do", "BBSMSTR_000000002612");
         targetScraping("상계중", "https://sanggye.sen.ms.kr/16536/subMenu.do", "https://sanggye.sen.ms.kr/dggb/module/board/selectBoardListAjax.do", "BBSMSTR_000000005204");
         targetScraping("창동중", "https://changdong.sen.ms.kr/17199/subMenu.do", "https://changdong.sen.ms.kr/dggb/module/board/selectBoardListAjax.do", "BBSMSTR_000000005441");
         targetScraping("상천초", "https://sangcheon.sen.es.kr/73576/subMenu.do", "https://sangcheon.sen.es.kr/dggb/module/board/selectBoardListAjax.do", "BBSMSTR_000000010373");
+
+        targetScraping("신상중", "https://sinsang.sen.ms.kr/76919/subMenu.do", "https://sinsang.sen.ms.kr/dggb/module/board/selectBoardListAjax.do", "BBSMSTR_000000011678");
+        targetScraping("상계제일중", "https://sanggyejeil.sen.ms.kr/75585/subMenu.do", "https://sanggyejeil.sen.ms.kr/dggb/module/board/selectBoardListAjax.do", "BBSMSTR_000000011156");
+        targetScraping("상원중", "https://sangwon.sen.ms.kr/12156/subMenu.do", "https://sangwon.sen.ms.kr/dggb/module/board/selectBoardListAjax.do", "BBSMSTR_000000003512");
 
         log.info("sch end");
     }
@@ -65,12 +70,15 @@ public class Scheduler {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             LocalDate givenDate = LocalDate.parse(noticeDto.getDate(), formatter);
 
-            LocalDate specificDate = LocalDate.parse("2023-12-29", formatter);
+            LocalDate specificDate = LocalDate.now();
 
-            if (givenDate.isAfter(specificDate)) {
+            if (givenDate.isEqual(specificDate)) {
                 String content = String.format("제목 : %s \n날짜 : %s \n링크 : %s", noticeDto.getTitle(), noticeDto.getDate(), link);
+
                 EmailSender.send("hsj@infotech.co.kr", targetName + " NEW 공지사항", content);
                 EmailSender.send("ljhforyou@ckdpharm.com", targetName + " NEW 공지사항", content);
+                EmailSender.send("kjh91644412@gmail.com", targetName + " NEW 공지사항", content);
+
                 log.info(targetName + "\n" + content);
             }
         }
